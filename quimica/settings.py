5b1bcf9fcd2b5908a7b87edf2185c7535c38905a
eduardolimasoares <eduardolimasoares@gmail.com>
2021-05-28 01:55:35 -0300

    finalizado

--- quimica/settings.py
+++ quimica/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-u0gh_05_-sv$xhgbth*zewp_hkdp$=%ji%(9m_@_y$7+%+coai
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
